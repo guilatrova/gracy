8d0147613c83c708064d05033ba1e7a24d3fa6cf
guilatrova <hello@guilatrova.dev>
2023-01-28 08:28:31 -0300

    feat: display status range in metrics

--- src/gracy/reports.py
+++ src/gracy/reports.py
@@ -43,15 +43,21 @@ class GracyReport:
         table.add_column("Failed Requests (%)", justify="right")
         table.add_column("Avg Latency (s)", justify="right")
         table.add_column("Max Latency (s)", justify="right")
+        table.add_column("2xx Responses", justify="right")
+        table.add_column("3xx Responses", justify="right")
+        table.add_column("4xx Responses", justify="right")
+        table.add_column(">5xx Responses", justify="right")
 
         for url, data in requests_sum.items():
             all_requests = requests_by_url[url]
-            url_latency = [r.elapsed.total_seconds() for r in all_requests]
 
+            # Latency
+            url_latency = [r.elapsed.total_seconds() for r in all_requests]
             avg_latency = mean(url_latency)
             max_latency = max(url_latency)
-            total_requests = data["total"]
 
+            # Total and Rates
+            total_requests = data["total"]
             successful_requests = sum(
                 [count for status, count in data.items() if status != "total" and 200 <= status.value < 300]
             )
@@ -60,6 +66,14 @@ class GracyReport:
             failed_rate = (failed_requests / total_requests) * 100
             failed_color = "red" if failed_requests else "green"
 
+            # Status Ranges
+            # fmt:off
+            responses_2xx = sum(count for status, count in data.items() if status != "total" and 200 <= status.value < 300)  # noqa: E501
+            responses_3xx = sum(count for status, count in data.items() if status != "total" and 300 <= status.value < 400)  # noqa: E501
+            responses_4xx = sum(count for status, count in data.items() if status != "total" and 400 <= status.value < 500)  # noqa: E501
+            responses_5xx = sum(count for status, count in data.items() if status != "total" and 500 <= status.value)
+            # fmt:on
+
             table.add_row(
                 url,
                 f"[bold]{total_requests}[/bold]",
@@ -67,6 +81,10 @@ class GracyReport:
                 f"[bold][{failed_color}]{failed_rate:.2f}%[/bold][/{failed_color}]",
                 f"{avg_latency:.2f}",
                 f"{max_latency:.2f}",
+                str(responses_2xx),
+                str(responses_3xx),
+                str(responses_4xx),
+                str(responses_5xx),
             )
 
             console.print(table)
