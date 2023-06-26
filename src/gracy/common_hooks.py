3feef652b1de9aa32b551c54307965f24a92e1b7
guilatrova <hello@guilatrova.dev>
2023-06-26 10:50:01 -0300

    feat(hooks): handle dates for retry_after header

--- src/gracy/common_hooks.py
+++ src/gracy/common_hooks.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 import asyncio
+import logging
 import typing as t
 from asyncio import Lock
+from datetime import datetime
 from http import HTTPStatus
 
 import httpx
@@ -10,6 +12,8 @@ import httpx
 from ._loggers import do_log, extract_base_format_args, extract_response_format_args
 from ._models import GracyRequestContext, LogEvent
 from ._reports._builders import ReportBuilder
+
+logger = logging.getLogger("gracy")
 
 
 class HttpHeaderRetryAfterBackOffHook:
@@ -39,7 +43,7 @@ class HttpHeaderRetryAfterBackOffHook:
         self._lock = Lock()
         self._log_event = log_event
 
-    def _process_log(self, request_context: GracyRequestContext, response: httpx.Response, retry_after: int) -> None:
+    def _process_log(self, request_context: GracyRequestContext, response: httpx.Response, retry_after: float) -> None:
         if event := self._log_event:
             format_args: t.Dict[str, str] = dict(
                 **extract_base_format_args(request_context),
@@ -48,6 +52,27 @@ class HttpHeaderRetryAfterBackOffHook:
             )
 
             do_log(event, self.DEFAULT_LOG_MESSAGE, format_args, response)
+
+    def _parse_retry_after_as_seconds(self, response: httpx.Response) -> float:
+        retry_after_value = response.headers.get("retry-after")
+
+        if retry_after_value is None:
+            return 0
+
+        if retry_after_value.isdigit():
+            return int(retry_after_value)
+
+        try:
+            # It might be a date as: Wed, 21 Oct 2015 07:28:00 GMT
+            date_time = datetime.strptime(retry_after_value, "%a, %d %b %Y %H:%M:%S %Z")
+            date_as_seconds = (date_time - datetime.now()).total_seconds()
+
+        except Exception:
+            logger.exception(f"Unable to parse {retry_after_value} as date within {type(self).__name__}")
+            return 0
+
+        else:
+            return date_as_seconds
 
     async def before(self):
         while self._lock.locked():
@@ -59,11 +84,10 @@ class HttpHeaderRetryAfterBackOffHook:
         response_or_exc: httpx.Response | Exception,
     ):
         if isinstance(response_or_exc, httpx.Response) and response_or_exc.status_code == HTTPStatus.TOO_MANY_REQUESTS:
-            retry_after = int(response_or_exc.headers.get("retry-after", 0))
-            # TODO: This might be a date
+            retry_after_seconds = self._parse_retry_after_as_seconds(response_or_exc)
 
-            if retry_after > 0:
+            if retry_after_seconds > 0:
                 async with self._lock:
                     self._reporter.throttled(context)
-                    self._process_log(context, response_or_exc, retry_after)
-                    await asyncio.sleep(retry_after)
+                    self._process_log(context, response_or_exc, retry_after_seconds)
+                    await asyncio.sleep(retry_after_seconds)
