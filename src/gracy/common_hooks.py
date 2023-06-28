a5f043dc4a93fa2ade28967d8b019e483d52c6f7
guilatrova <hello@guilatrova.dev>
2023-06-28 08:58:39 -0300

    feat: allow to modify retry-after

--- src/gracy/common_hooks.py
+++ src/gracy/common_hooks.py
@@ -37,28 +37,40 @@ class HttpHeaderRetryAfterBackOffHook:
     ### Throttling
     It takes the reporter, because it counts every await as "throttled" for that UURL.
 
+    ### Processor
+    You can optionally pass in a lambda, that can be used to modify/increase the wait time from the header.
+
     ### Log Event
     You can optionally define a log event.
     It provides the response and the context, but also `RETRY_AFTER` that contains the header value.
+    `RETRY_AFTER_ACTUAL_WAIT` is also available in case you modify the original value.
     """
 
     DEFAULT_LOG_MESSAGE: t.Final = "[{METHOD}] {URL} requested to wait for {RETRY_AFTER}s"
     ALL_CLIENT_LOCK: t.Final = "CLIENT"
 
     def __init__(
-        self, reporter: ReportBuilder, lock_per_endpoint: bool = False, log_event: LogEvent | None = None
+        self,
+        reporter: ReportBuilder,
+        lock_per_endpoint: bool = False,
+        log_event: LogEvent | None = None,
+        seconds_processor: None | t.Callable[[float], float] = None,
     ) -> None:
         self._reporter = reporter
         self._lock_per_endpoint = lock_per_endpoint
         self._lock_manager = t.DefaultDict[str, Lock](Lock)
         self._log_event = log_event
+        self._processor = seconds_processor or (lambda x: x)
 
-    def _process_log(self, request_context: GracyRequestContext, response: httpx.Response, retry_after: float) -> None:
+    def _process_log(
+        self, request_context: GracyRequestContext, response: httpx.Response, retry_after: float, actual_wait: float
+    ) -> None:
         if event := self._log_event:
             format_args: t.Dict[str, str] = dict(
                 **extract_base_format_args(request_context),
                 **extract_response_format_args(response),
                 RETRY_AFTER=str(retry_after),
+                RETRY_AFTER_ACTUAL_WAIT=str(actual_wait),
             )
 
             do_log(event, self.DEFAULT_LOG_MESSAGE, format_args, response)
@@ -94,14 +106,17 @@ class HttpHeaderRetryAfterBackOffHook:
     ) -> HookResult:
         if isinstance(response_or_exc, httpx.Response) and response_or_exc.status_code == HTTPStatus.TOO_MANY_REQUESTS:
             retry_after_seconds = self._parse_retry_after_as_seconds(response_or_exc)
+            actual_wait = self._processor(retry_after_seconds)
 
             if retry_after_seconds > 0:
                 lock_name = context.unformatted_url if self._lock_per_endpoint else self.ALL_CLIENT_LOCK
 
                 async with self._lock_manager[lock_name]:
                     self._reporter.throttled(context)
-                    self._process_log(context, response_or_exc, retry_after_seconds)
-                    await asyncio.sleep(retry_after_seconds)
+                    self._process_log(context, response_or_exc, retry_after_seconds, actual_wait)
+
+                    await asyncio.sleep(actual_wait)
+
                     return HookResult(True)
 
         return HookResult(False)
